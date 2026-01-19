2c2e991df64e5b879f0c985bb6b235784e4ccd2c
ZiyadCh <chetoziyad@gmail.com>
2026-01-19 15:14:06 +0100

    modified some ./

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -1,6 +1,6 @@
 <h2>Register</h2>
 
-<form method="POST" action="/register">
+<form method="POST" action="./register">
     <input type="text" name="first_name" placeholder="First name" required>
 
     <input type="text" name="last_name" placeholder="Last name" required>
@@ -13,5 +13,5 @@
 </form>
 
 <div class="link">
-    <a href="/login">Already have an account?</a>
+  <a href="./login">Already have an account?</a>
 </div>
