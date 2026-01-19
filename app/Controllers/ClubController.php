19aa0e92ff80a03c39839e4bf5c140f4e065bf18
amineElgaini <elgainiamine@gmail.com>
2026-01-19 12:33:12 +0100

    udpate index.php put the routes, Add controllers

---
+++ app/Controllers/ClubController.php
@@ -0,0 +1,9 @@
+<?php
+// app/Controllers/ClubController.php
+class ClubController {
+    public function index() {}       // show all clubs
+    public function show($id) {}    // show single club
+    public function store() {}       // create new club (admin)
+    public function update($id) {}  // update club (admin)
+    public function destroy($id) {} // delete club (admin)
+}
---
+++ app/Controllers/EventController.php
@@ -0,0 +1,8 @@
+<?php
+// app/Controllers/EventController.php
+class EventController {
+    public function index() {}                                   // list all events
+    public function register($id) {}                             // student registers for event
+    public function update($id) {}                               // update event (president)
+    public function destroy($id) {}                              // delete event (president)
+}
